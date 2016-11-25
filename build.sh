ee6f58232e526768ad4086b87a3912d6e349623e
Arto Santala <arto.santala@solita.fi>
2016-11-25 13:48:23 +0200

    Fixed issue with build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 mvn -f kontitpomppimaan/pom.xml clean package
-(cd kontitpomppimaan/src/main/resources/static && npm install && npm tsc)
+(cd kontitpomppimaan/src/main/resources/static && npm install && npm run tsc)
 docker-compose build
